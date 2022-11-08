d7aadb76bca69289c0ff6c6371004fb0688ee7b9
ndiazdossantos <55813329+ndiazdossantos@users.noreply.github.com>
2022-11-08 18:00:32 +0100

    finalizado ejercicio6

---
+++ ejercicio6/proceso.php
@@ -0,0 +1,10 @@
+<?php
+$nombre = $_REQUEST['nombre'];
+$grupo = $_REQUEST['grupo'];
+$comida = $_REQUEST['comida'];
+
+echo "Estos son os gustos de $nombre"."<br/>";
+echo "A súa música preferida é $grupo e a súa comida preferida é a $comida";
+
+
+?>
