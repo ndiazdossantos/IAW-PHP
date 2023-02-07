609b1bd61ec6189cda57fad7a7bd01dd996ea571
ndiazdossantos <55813329+ndiazdossantos@users.noreply.github.com>
2023-02-07 18:53:03 +0100

    pendiente revisar administracion

--- sitioWeb/administracion.php
+++ sitioWeb/administracion.php
@@ -1,1 +1,47 @@
 <?php
+session_start();
+echo "<br><div align='right'><b>Usuario:</b> ".$_SESSION["usuario"]."</div><br>";
+$user = $_SESSION["usuario"];
+
+$mysqli_link = mysqli_connect("127.0.0.1", "root","", "frota");
+
+    if (mysqli_connect_errno())
+    {
+        printf("A conexion con MySQL fallou co erro: %s",
+            mysqli_connect_error());
+        exit;
+    }
+    if (isset($_REQUEST['admitir'])) {
+
+        $select_query2 = "SELECT * FROM novo_rexistro";
+        $result2 = mysqli_query($mysqli_link, $select_query2);
+        $valor = mysqli_fetch_array($result2, MYSQLI_ASSOC);
+        $numfilas2 = $result2->num_rows;
+
+        while ($valor = mysqli_fetch_array($result2, MYSQLI_ASSOC)) {
+
+            $usuario= $valor['usuario'];
+            $contrasinal= $valor['contrasinal'];
+            $nome=$valor['nome'];
+            $direccion=$valor['direccion'];
+            $telefono=$valor['telefono'];
+            $nifdni=$valor['nifdni'];
+            $email=$valor['email'];
+
+            $select_query = "SELECT * FROM usuario where usuario='$usuario'";
+            $resultado = mysqli_query($mysqli_link, $select_query2);
+            $numfilas = $resultado->num_rows;
+
+            if($numfilas>0){
+                echo "Xa existe o usuario, imposible inserción <br>";
+            }else{
+                $insert_query = "INSERT INTO usuario (usuario, contrasinal, nome, direccion, telefono, nifdni, email, tipo_usuario) VALUES ('$usuario', '$contrasinal', '$nome', '$direccion', '$telefono', '$nifdni', '$email','u')";
+                $insert = mysqli_query($mysqli_link, $insert_query);
+                $delete_query = "DELETE FROM novo_usuario where usuario='$usuario'";
+                $delete = mysqli_query($mysqli_link, $delete_query);
+
+            }
+
+        }
+    }
+    mysqli_close($mysqli_link);
--- sitioWeb/configuracion.php
+++ sitioWeb/configuracion.php
@@ -35,76 +35,76 @@ echo "<br><div align='right'><b>Usuario:</b> ".$_SESSION["usuario"]."</div><br>"
 
 $user = $_SESSION["usuario"];
 
-if(isset($_REQUEST['contrasinal_novo']) && isset($_REQUEST['nome_novo']) && isset($_REQUEST['direccion_novo']) && isset($_REQUEST['telefono_novo']) && isset($_REQUEST['nifdni_novo']) && isset($_REQUEST['email_novo'])){
+    if(isset($_REQUEST['contrasinal_novo']) && isset($_REQUEST['nome_novo']) && isset($_REQUEST['direccion_novo']) && isset($_REQUEST['telefono_novo']) && isset($_REQUEST['nifdni_novo']) && isset($_REQUEST['email_novo'])){
 
-    $contrasinal = $_REQUEST['contrasinal_novo'];
-    $nome = $_REQUEST['nome_novo'];
-    $direccion = $_REQUEST['direccion_novo'];
-    $telefono= $_REQUEST['telefono_novo'];
-    $nifdni = $_REQUEST['nifdni_novo'];
-    $email = $_REQUEST['email_novo'];
+        $contrasinal = $_REQUEST['contrasinal_novo'];
+        $nome = $_REQUEST['nome_novo'];
+        $direccion = $_REQUEST['direccion_novo'];
+        $telefono= $_REQUEST['telefono_novo'];
+        $nifdni = $_REQUEST['nifdni_novo'];
+        $email = $_REQUEST['email_novo'];
 
-}
+    }
 
 
-//establecemos conexion
-$mysqli_link = mysqli_connect("127.0.0.1", "root","", "frota");
-if (mysqli_connect_errno())
-{
-    printf("A conexion con MySQL fallou co erro: %s",
-        mysqli_connect_error());
-    exit;
-}
-//comprobación de si los campos de registro están vacía
+    //establecemos conexion
+    $mysqli_link = mysqli_connect("127.0.0.1", "root","", "frota");
+    if (mysqli_connect_errno())
+    {
+        printf("A conexion con MySQL fallou co erro: %s",
+            mysqli_connect_error());
+        exit;
+    }
+    //comprobación de si los campos de registro están vacía
 
-mysqli_set_charset($mysqli_link,"utf8");
+    mysqli_set_charset($mysqli_link,"utf8");
 
-if (!empty($contrasinal)){
+    if (!empty($contrasinal)){
 
-    echo '<br> Contrasinal actualizado';
-    $update_query = "UPDATE usuario SET contrasinal='$contrasinal' where usuario='$user'";
-    $update = mysqli_query($mysqli_link, $update_query);
+        echo '<br> Contrasinal actualizado';
+        $update_query = "UPDATE usuario SET contrasinal='$contrasinal' where usuario='$user'";
+        $update = mysqli_query($mysqli_link, $update_query);
 
-if (!empty($nome)){
+    if (!empty($nome)){
 
-    echo '<br> Nome actualizado';
-    $update_query = "UPDATE usuario SET nome='$nome' where usuario='$user'";
-    $update = mysqli_query($mysqli_link, $update_query);
+        echo '<br> Nome actualizado';
+        $update_query = "UPDATE usuario SET nome='$nome' where usuario='$user'";
+        $update = mysqli_query($mysqli_link, $update_query);
 
-}
+    }
 
-if (!empty($direccion)){
-    echo '<br> Direccion actualizada';
-    $update_query = "UPDATE usuario SET direccion='$direccion' where usuario='$user'";
-    $update = mysqli_query($mysqli_link, $update_query);
-}
+    if (!empty($direccion)){
+        echo '<br> Direccion actualizada';
+        $update_query = "UPDATE usuario SET direccion='$direccion' where usuario='$user'";
+        $update = mysqli_query($mysqli_link, $update_query);
+    }
 
-if (!empty($telefono)){
+    if (!empty($telefono)){
 
-    echo '<br> Telefono actualizado';
-    $update_query = "UPDATE usuario SET telefono='$telefono' where usuario='$user'";
-    $update = mysqli_query($mysqli_link, $update_query);
+        echo '<br> Telefono actualizado';
+        $update_query = "UPDATE usuario SET telefono='$telefono' where usuario='$user'";
+        $update = mysqli_query($mysqli_link, $update_query);
 
-}
+    }
 
-if (!empty($nifdni)){
+    if (!empty($nifdni)){
 
-    echo ' <br> Nif & DNI actualizado';
-    $update_query = "UPDATE usuario SET nifdni='$nifdni' where usuario='$user'";
-    $update = mysqli_query($mysqli_link, $update_query);
+        echo ' <br> Nif & DNI actualizado';
+        $update_query = "UPDATE usuario SET nifdni='$nifdni' where usuario='$user'";
+        $update = mysqli_query($mysqli_link, $update_query);
 
-}
+    }
 
-if (!empty($email)){
+    if (!empty($email)){
 
-    echo '<br> Email actualizado';
-    $update_query = "UPDATE usuario SET email='$email' where usuario='$user'";
-    $update = mysqli_query($mysqli_link, $update_query);
+        echo '<br> Email actualizado';
+        $update_query = "UPDATE usuario SET email='$email' where usuario='$user'";
+        $update = mysqli_query($mysqli_link, $update_query);
 
-}
+    }
 
-    mysqli_close($mysqli_link);
-}
+        mysqli_close($mysqli_link);
+    }
 
 
 ?>
